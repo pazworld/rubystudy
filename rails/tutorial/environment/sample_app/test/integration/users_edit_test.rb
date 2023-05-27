52a1d32fc35fb6071db2749c6b828d07f5dfabad
pazworld <paz@moudamepo.net>
2023-05-27 15:20:24 +0900

    support friendly forwarding

--- rails/tutorial/environment/sample_app/test/integration/users_edit_test.rb
+++ rails/tutorial/environment/sample_app/test/integration/users_edit_test.rb
@@ -17,10 +17,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', "The form contains 4 errors."
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
@@ -29,6 +29,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
+    assert_not session[:forwarding_url]
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
