a3e3c90abe6b7755274566394344a73d8f5d37bf
pazworld <paz@moudamepo.net>
2023-05-31 19:50:49 +0900

    redirect to root url after create user

--- rails/tutorial/environment/sample_app/test/integration/users_signup_test.rb
+++ rails/tutorial/environment/sample_app/test/integration/users_signup_test.rb
@@ -23,9 +23,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template "users/show"
-    assert_select "div#error_explanation", false
-    assert_select "div.alert-success"
-    assert is_logged_in?
+    #assert_template "users/show"
+    #assert_select "div#error_explanation", false
+    #assert_select "div.alert-success"
+    #assert is_logged_in?
   end
 end
