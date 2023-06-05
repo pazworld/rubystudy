d6902def057c046aa5f04f7239bb4002d0fd7909
pazworld <paz@moudamepo.net>
2023-06-05 18:56:31 +0900

    users page not contain inactive user

--- rails/tutorial/environment/sample_app/test/integration/users_index_test.rb
+++ rails/tutorial/environment/sample_app/test/integration/users_index_test.rb
@@ -25,7 +25,7 @@ class UsersIndexAdminTest < UsersIndexAdmin
   end
 
   test "should have delete links" do
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
@@ -39,6 +39,19 @@ class UsersIndexAdminTest < UsersIndexAdmin
       delete user_path(@non_admin)
       assert_response :see_other
       assert_redirected_to users_url
+    end
+  end
+
+  test "should display only activated users" do
+    # ページにいる最初のユーザーを無効化する。
+    # 無効なユーザーを作成するだけでは、
+    # Railsで最初のページに表示される保証がないので不十分
+    User.paginate(page: 1).first.toggle!(:activated)
+    # /usersを再度取得して、無効化済みのユーザーが表示されていないことを確かめる
+    get users_path
+    # 表示されているすべてのユーザーが有効化済みであることを確かめる
+    assigns(:users).each do |user|
+      assert user.activated?
     end
   end
 end
