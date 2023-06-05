dbb9abb9cd5fd9c20b774aff81ed3f571440e16b
pazworld <paz@moudamepo.net>
2023-06-05 18:45:12 +0900

    refactor users index tests divided in several classes

--- rails/tutorial/environment/sample_app/test/integration/users_index_test.rb
+++ rails/tutorial/environment/sample_app/test/integration/users_index_test.rb
@@ -1,16 +1,30 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
   end
+end
 
-  test "index as admin including pagination and delete links" do
+class UsersIndexAdmin < UsersIndex
+  def setup
+    super
     log_in_as(@admin)
     get users_path
+  end
+end
+
+class UsersIndexAdminTest < UsersIndexAdmin
+  test "should render the index page" do
     assert_template 'users/index'
-    assert_select 'div.pagination', count: 2
+  end
+
+  test "should paginate users" do
+    assert_select 'div.pagination'
+  end
+
+  test "should have delete links" do
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
@@ -18,14 +32,19 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
+  end
+
+  test "should be able to delete non-admin user" do
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
       assert_response :see_other
       assert_redirected_to users_url
     end
   end
+end
 
-  test "index as non-admin" do
+class UsersNonAdminIndexTest < UsersIndex
+  test "should not have delete links as non-admin" do
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
